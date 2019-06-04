21c2b3304a38bd0b2e460d7d3f9421e1fc796db7
kousei kimura <k.kimura1811@morijyobi.ac.jp>
2019-06-04 11:23:23 +0900

    [modify] Conflict.javaを編集

--- Conflict.java
+++ Conflict.java
@@ -1,5 +1,12 @@
 public class Conflict{
         public static void main(String... args){
-		System.out.println("Hello , World!");
+		
+               for (int i = 1; i <= 30; i++) {
+        		
+                    System.out.println(i);
+        
+        	}
+
 	}                                            
 }
+
